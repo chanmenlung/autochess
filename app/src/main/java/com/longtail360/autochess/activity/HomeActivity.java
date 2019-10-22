291d880e0ad34677d76723cb95b7f0fbe413a220
chanmenlung <loichui@gmail.com>
2019-10-22 23:25:32 +0800

    Initial commit

---
+++ app/src/main/java/com/longtail360/autochess/activity/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.longtail360.autochess.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.longtail360.autochess.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
---
+++ app/src/main/java/com/longtail360/autochess/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.longtail360.autochess.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.longtail360.autochess.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
